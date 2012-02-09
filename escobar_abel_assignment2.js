2999071c0d51bd69f0fba0ee5360f2d7b0c07cce
Abel <absdrmr81@fullsail.edu>
2012-02-09 17:25:29 -0600

    Added procedure to repo.

--- escobar_abel_assignment2.js
+++ escobar_abel_assignment2.js
@@ -1,1 +1,18 @@
-alert("JavaScript works!");
+// Author: Abel Escobar
+// Date: 02/09/12
+// Title: Deliverable 2
+// Description: Clean up time
+
+console.log("So it was that time of the year again; it was the dreadful day of spring cleaning.");
+console.log("My girlfriend and I did not want to clean the attic, but someone had to do it.");
+console.log("So she bet me that if I had enough change in my pocket to buy her favorite candybar, then I wouldn't have to clean the attic.");
+console.log("I started to count the money I had in my pockets.");
+
+
+var getFrontPocketMoney = function (t) {
+	var money = t 
+	console.log(money)
+};
+
+getFrontPocketMoney(.67);
+getFrontPocketMoney(.31);
